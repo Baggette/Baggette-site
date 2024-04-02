97abea91f130ded49b990323e588c3f2aca8ff15
baggette <58569691+Baggette@users.noreply.github.com>
2024-04-01 23:26:03 -0500

    fix

--- js/counter.js
+++ js/counter.js
@@ -1,1 +1,4 @@
-const url = '/counter.txt'; fetch(url).then(r => r.text()).then(t => {const count = document.getElementById('visitors').innerHTML = 'Visitors: ' + t});
+function visitors() {
+	const url = '/counter.txt'
+	fetch(url).then(r => r.text()).then(t => {const count = document.getElementById('visitors').innerHTML = 'Visitors: ' + t});
+}

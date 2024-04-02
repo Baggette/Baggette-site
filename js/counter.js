b2f5466b29b1c044fadbb0e4a537bc3de9798204
baggette <58569691+Baggette@users.noreply.github.com>
2024-04-01 23:31:20 -0500

    forgot to actually call the function

--- js/counter.js
+++ js/counter.js
@@ -2,3 +2,4 @@ function visitors() {
 	const url = '/counter.txt'
 	fetch(url).then(r => r.text()).then(t => {const count = document.getElementById('visitors').innerHTML = 'Visitors: ' + t});
 }
+visitors();
